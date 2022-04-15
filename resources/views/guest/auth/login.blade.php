3f95d62bdd1103db56e6b26dc988545a52a0dfcc
Riyaldi Hasan <riy.2923@gmail.com>
2022-04-15 11:16:00 +0700

    change ui login

--- resources/views/guest/auth/login.blade.php
+++ resources/views/guest/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Register</title>
+    <title>Log In</title>
 
     <link href="css/app.css" rel="stylesheet">
 
@@ -34,12 +34,25 @@
             padding-top: 40px;
             padding-bottom: 40px;
             background-color: #f5f5f5;
+            background-image: url('https://images.unsplash.com/photo-1445019980597-93fa8acb246c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-position: center;
+            background-size: cover;
+        }
+
+        .login {
+            height: 100vh;
+            width: 100vw;
+            display: flex;
+            align-items: center;
+            background-color: #00000055;
         }
 
         .form-signin {
             width: 100%;
-            max-width: 330px;
-            padding: 15px;
+            max-width: 360px;
+            padding: 32px;
             margin: auto;
         }
 
@@ -63,6 +76,20 @@
             border-top-right-radius: 0;
         }
 
+        .form-signin button {
+            border: 0 0 0 !important;
+            border-color: #fff !important;
+            border-radius: 0 !important;
+            background-color: #f3c300;
+            color: #fff;
+        }
+
+        .form-signin button:hover {
+            border: 0 0 0 !important;
+            border-color: #fff !important;
+            background-color: #dbb314;
+        }
+
     </style>
 
 
@@ -71,31 +98,31 @@
 </head>
 
 <body class="text-center">
+    <div class="login">
 
-    <main class="form-signin">
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-            <img class="mb-4"
-                src="https://img.okezone.com/content/2022/03/03/45/2555733/manchester-city-lupakan-harry-kane-lakukan-pendekatan-dengan-erling-haaland-tdYNo1Gesw.jpg"
-                alt="" width="102" height="77">
-            <h1 class="h3 mb-3 fw-normal">Login masseh</h1>
+        <main class="form-signin card">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <h1 class="h3 mb-4 mt-2 fw-normal">Log In</h1>
 
-            <div class="form-floating">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
-                    name="email">
-                <label for="floatingInput">Email address masseh</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
-                    name="password">
-                <label for="floatingPassword">Password masseh</label>
-            </div>
+                <div class="form-floating">
+                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+                        name="email">
+                    <label for="floatingInput">Email</label>
+                </div>
+                <div class="form-floating">
+                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
+                        name="password">
+                    <label for="floatingPassword">Password</label>
+                </div>
 
-            <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Login masseh</button>
-            <p class="mt-5 mb-3 text-muted">2022</p>
-        </form>
-    </main>
+                <button class="w-100 btn btn-lg btn-primary mt-4 mb-4" type="submit">Log In</button>
 
+                <a href="{{ route('register') }}">Don't have any account?</a>
+            </form>
+        </main>
+
+    </div>
 </body>
 
 </html>
