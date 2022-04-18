270175a36f5b2de2aad5ecdc83a4be4df8b2c8ac
Riyaldi Hasan <riy.2923@gmail.com>
2022-04-18 10:07:04 +0700

    fix bug frontend add hotel facility

--- resources/views/admin/hotel_facility/create.blade.php
+++ resources/views/admin/hotel_facility/create.blade.php
@@ -2,6 +2,9 @@
 @section('content')
     <section class="section">
         <div class="row">
+            <div class="col-12 col-md-2 col-lg-2">
+            </div>
+
             <div class="col-12 col-md-8 col-lg-8">
                 <div class="card">
                     <div class="card-header" style="width: 100%;">
@@ -33,18 +36,7 @@
                 </div>
             </div>
 
-            <div class="col-12 col-md-4 col-lg-4">
-                <div class="card">
-                    <div class="card-header" style="width: 100%;">
-                        <h4>Add New Image</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="form-group">
-                            <label>Upload new image</label>
-                            <input type="file" class="form-control" name="image" form="hotel_facility_form">
-                        </div>
-                    </div>
-                </div>
+            <div class="col-12 col-md-2 col-lg-2">
             </div>
         </div>
     </section>
